9edbcaf4c3c151386bf7a53bf66682c311b4d6c0
Reese Hudson <david.reese.hudson@gmail.com>
2020-11-03 13:46:25 -0500

    first sql success

--- index.php
+++ index.php
@@ -2,21 +2,39 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$database = "sqlHeroes";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//total list of heroes
+$list_of_heroes = "SELECT name FROM heroes";
+$result = $conn->query($list_of_heroes);
+
+$output = "";
+
+if ($result -> num_rows > 0) {
+    // output data of each row
+    while ($row = $result -> fetch_assoc()) {
+        $output .= "<li>" . "<a href='#'>" . $row["name"] . "</a>" . "</li>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
 ?>
+
 
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <title>City of Heroes</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -24,13 +42,31 @@ echo "Connected successfully";
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-      
+</head>
+
+<body>
+    <h1>City of Heroes</h1>
+    <ul>
+
+        <?php
+            echo $output;
+        ?>
+
+    </ul>
+
+    <!-- 
+        profile of selected hero/heroine
+
+        button to add relationship
+
+        form to add new hero/heroine -->
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
